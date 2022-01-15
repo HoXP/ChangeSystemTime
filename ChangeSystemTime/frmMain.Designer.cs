1ad9276ebd15806cc201555a19afb1548b02bf9d
Ho <hxptow@163.com>
2022-01-15 12:12:38 +0800

    修改Windows系统时间

---
+++ ChangeSystemTime/frmMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ChangeSystemTime
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChange = new System.Windows.Forms.Button();
+            this.txbTime = new System.Windows.Forms.TextBox();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(11, 39);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(457, 42);
+            this.btnChange.TabIndex = 0;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // txbTime
+            // 
+            this.txbTime.Location = new System.Drawing.Point(12, 12);
+            this.txbTime.Name = "txbTime";
+            this.txbTime.Size = new System.Drawing.Size(456, 21);
+            this.txbTime.TabIndex = 8;
+            this.txbTime.Text = "2021/05/30 10:00:00";
+            // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Location = new System.Drawing.Point(13, 88);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(35, 12);
+            this.lblDone.TabIndex = 9;
+            this.lblDone.Text = "Ready";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 111);
+            this.Controls.Add(this.lblDone);
+            this.Controls.Add(this.txbTime);
+            this.Controls.Add(this.btnChange);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 150);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change System Time";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox txbTime;
+        private System.Windows.Forms.Label lblDone;
+    }
+}
+
---
+++ ChangeSystemTime/frmMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ChangeSystemTime
+{
+    public partial class frmMain : Form
+    {
+        //设置系统时间的API函数
+        [DllImport("kernel32.dll")]
+        private static extern bool SetLocalTime(ref SYSTEMTIME time);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SYSTEMTIME
+        {
+            public short year;
+            public short month;
+            public short dayOfWeek;
+            public short day;
+            public short hour;
+            public short minute;
+            public short second;
+            public short milliseconds;
+        }
+
+        private string timeFormat = "yyyy-MM-dd HH:mm:ss";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 设置系统时间
+        /// </summary>
+        /// <param name="dt">需要设置的时间</param>
+        /// <returns>返回系统时间设置状态，true为成功，false为失败</returns>
+        public bool SetDate(DateTime dt)
+        {
+            SYSTEMTIME st;
+            st.year = (short)dt.Year;
+            st.month = (short)dt.Month;
+            st.dayOfWeek = (short)dt.DayOfWeek;
+            st.day = (short)dt.Day;
+            st.hour = (short)dt.Hour;
+            st.minute = (short)dt.Minute;
+            st.second = (short)dt.Second;
+            st.milliseconds = (short)dt.Millisecond;
+            bool rt = SetLocalTime(ref st);
+            return rt;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txbTime.Text = DateTime.Now.ToString(timeFormat);
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            string strTime = txbTime.Text;
+            if (SetDate(DateTime.Parse(strTime)))
+            {
+                lblDone.Text = string.Format($"Succeed - {DateTime.Now.ToString(timeFormat)}");
+            }
+            else
+            {
+                lblDone.Text = string.Format($"Failed - {DateTime.Now.ToString(timeFormat)}");
+            }
+        }
+    }
+}
